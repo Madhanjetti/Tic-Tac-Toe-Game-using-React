edcd835d3a35896422325b4d18d9f40eede95bb6
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2023-07-20 23:18:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Card from './components/Card/Card';
+import Grid from './components/Grid/Grid';
+import Icon from './components/Icon/Icon';
+
+function App() {
+ 
+
+  return (
+    <>
+    
+     <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
